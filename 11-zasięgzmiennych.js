ab062ff200ea75a6ec74e3813ebbddcf195ae8a2
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 15:35:20 +0200

    zmienne i funkcje
    sumaKasy

--- 11-zasięgzmiennych.js
+++ 11-zasięgzmiennych.js
@@ -1,2 +1,20 @@
 'use strict'
 /*lokalna nadpisyje wartość globalną w przypadku gdy są takie same nazwy np. wart 1 globalna i wart1 lokalna*/
+
+
+var kszysiek = 3500;
+var jurek = 5000;
+var jola = 7500;
+var sergio = 2500;
+
+
+function policzSumeKasy(skladowa1, skladowa2, skladowa3, skladowa4){
+
+var sumaKasy;
+sumaKasy = skladowa1 + skladowa2 + skladowa3 +skladowa4;
+
+return sumaKasy;
+}
+
+sumaKasy = policzSumeKasy( krzysiek, jurek, jola, sergio );
+console.log(sumaKAsy);
