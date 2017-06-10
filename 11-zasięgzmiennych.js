03373b595be91ace89b0865f9c040a1a73a7e5f6
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 15:38:47 +0200

    finish

--- 11-zasięgzmiennych.js
+++ 11-zasięgzmiennych.js
@@ -2,7 +2,7 @@
 /*lokalna nadpisyje wartość globalną w przypadku gdy są takie same nazwy np. wart 1 globalna i wart1 lokalna*/
 
 
-var kszysiek = 3500;
+var krzysiek = 3500;
 var jurek = 5000;
 var jola = 7500;
 var sergio = 2500;
@@ -16,5 +16,5 @@ sumaKasy = skladowa1 + skladowa2 + skladowa3 +skladowa4;
 return sumaKasy;
 }
 
-sumaKasy = policzSumeKasy( krzysiek, jurek, jola, sergio );
+sumaKasy= policzSumeKasy( krzysiek, jurek, jola, sergio );
 console.log(sumaKAsy);
